050957ec6d073bcb0ef1be45ef4f2d039d735ed3
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2022-05-11 14:55:24 +0900

    add postCss

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: [
+		require('postcss-preset-env')({
+			stage: 3,
+      		browsers: "last 1 versions, > 1%",
+      		autoprefixer: { grid: false },
+      		features: {
+      			"custom-properties": false
+      		}
+		})
+	]
+}
